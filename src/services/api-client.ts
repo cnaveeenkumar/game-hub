dd9cd5b0ca8ca8590d46bbe6c9e46947339f6d86
Naveenkumar C <cnaveen777@gmail.com>
2025-01-09 12:03:09 +0530

    Update api-client.ts
    Hide the key and client ID for security reasons

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -5,9 +5,7 @@
 //     baseURL: 'https://api.unsplash.com/photos',
 //     //baseURL: "https://api.rawg.io/api",
 //     params: {
-//         key:'4KK0GFyzcZHMzT1Me-ZfW5nj9urzAttzDRu0j9O8HP8'
-//         //key:'f99cefd5ca8348a7b04c53f1389ba3d1'
-//         //key: "604775bd6ddd81527537686db1cafcf9",
+//         key:'***********************************'
 //     },
 // });
 
@@ -16,7 +14,7 @@
 // export default axios.create({
 //     baseURL: 'https://api.unsplash.com',
 //     params: {
-//         key: '4KK0GFyzcZHMzT1Me-ZfW5nj9urzAttzDRu0j9O8HP8'
+//         key: '*******************************************'
 //     }
 // });
 
@@ -25,7 +23,7 @@
 // export default axios.create({
 //     baseURL: 'https://api.unsplash.com',
 //     params: {
-//         client_id: '4KK0GFyzcZHMzT1Me-ZfW5nj9urzAttzDRu0j9O8HP8' // Use client_id here
+//         client_id: '***************************************' // Use client_id here
 //     }
 // });
 
@@ -34,10 +32,10 @@
 // export default axios.create({
 //     baseURL: 'https://developer.marvel.com',
 //     params: {
-//         key: 'eeb21a6b38b98d64251735eb5135a3e1223b618a' // Use Access Key here
+//         key: '*************************************' // Use Access Key here
 //     }
 //     // headers: {
-//     //     'Authorization': 'Client-ID 4KK0GFyzcZHMzT1Me-ZfW5nj9urzAttzDRu0j9O8HP8', // Access Key in the header
+//     //     'Authorization': 'Client-ID ******************************', // Access Key in the header
 //     //     'Application-ID': '654666' // Application ID in the header if needed
 //     // }
 // });
